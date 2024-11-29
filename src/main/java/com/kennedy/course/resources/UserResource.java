ce89250ef4f79c93b31ba1c578f825bbeeda48a3
kennedy_brito <willian.kennedy.brito@gmail.com>
2024-11-29 17:03:31 -0300

    feat: add getUserById ; new type of url, argument in the url

--- src/main/java/com/kennedy/course/resources/UserResource.java
+++ src/main/java/com/kennedy/course/resources/UserResource.java
@@ -2,9 +2,11 @@ package com.kennedy.course.resources;
 
 import com.kennedy.course.entities.User;
 import com.kennedy.course.servicies.UserService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +24,13 @@ public class UserResource {
     public ResponseEntity<List<User>> findAll(){
         List<User> u = service.findAll();
         return ResponseEntity.ok().body(u);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+
+        User user = service.findById(id);
+
+        return ResponseEntity.ok().body(user);
     }
 }
--- src/main/java/com/kennedy/course/servicies/UserService.java
+++ src/main/java/com/kennedy/course/servicies/UserService.java
@@ -6,14 +6,21 @@ import com.kennedy.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserRepository repository;
 
     public List<User> findAll(){
-        return userRepository.findAll();
+        return repository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = repository.findById(id);
+
+        return obj.get();
     }
 }
