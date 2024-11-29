b849446c2516766618abf735dcdf93526f0b7d13
kennedy_brito <willian.kennedy.brito@gmail.com>
2024-11-29 15:54:41 -0300

    feat: consume UserService in UserResource

--- src/main/java/com/kennedy/course/resources/UserResource.java
+++ src/main/java/com/kennedy/course/resources/UserResource.java
@@ -1,19 +1,26 @@
 package com.kennedy.course.resources;
 
 import com.kennedy.course.entities.User;
+import com.kennedy.course.servicies.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
-    @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User(1L, "Maria", "maria@gmail.com", "999999", "12345");
+    @Autowired
+    private UserService service;
 
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll(){
+        List<User> u = service.findAll();
         return ResponseEntity.ok().body(u);
     }
 }
