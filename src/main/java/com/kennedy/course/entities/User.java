9ea3dde87377effe5900c206907df44f98489d4e
kennedy_brito <willian.kennedy.brito@gmail.com>
2024-11-29 15:32:19 -0300

    feat: add User as db entity

--- src/main/java/com/kennedy/course/entities/User.java
+++ src/main/java/com/kennedy/course/entities/User.java
@@ -1,13 +1,19 @@
 package com.kennedy.course.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity //defines entity as a table
+@Table(name =  "tb_user") //renaming as tb_user
 public class User implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
