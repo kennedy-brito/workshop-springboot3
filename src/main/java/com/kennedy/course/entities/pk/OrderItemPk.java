7db607ca20baab41786beeb832e3149f55a4c6aa
kennedy_brito <willian.kennedy.brito@gmail.com>
2024-12-02 15:45:42 -0300

    feat: add OrderItem Primary Key object representation

---
+++ src/main/java/com/kennedy/course/entities/pk/OrderItemPk.java
@@ -0,0 +1,53 @@
+package com.kennedy.course.entities.pk;
+
+import com.kennedy.course.entities.Order;
+import com.kennedy.course.entities.Product;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+public class OrderItemPk implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof OrderItemPk that)) return false;
+        return Objects.equals(getOrder(), that.getOrder()) && Objects.equals(getProduct(), that.getProduct());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getOrder(), getProduct());
+    }
+}
