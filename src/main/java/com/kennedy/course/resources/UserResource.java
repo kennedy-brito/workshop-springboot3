a245d99f21045e0790bb828ce6f7af2adafa1e97
kennedy_brito <willian.kennedy.brito@gmail.com>
2024-12-03 14:06:08 -0300

    feat: add User insert correct request response code

--- src/main/java/com/kennedy/course/resources/UserResource.java
+++ src/main/java/com/kennedy/course/resources/UserResource.java
@@ -6,7 +6,9 @@ import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -34,6 +36,13 @@ public class UserResource {
     @PostMapping
     public ResponseEntity<User> insert(@RequestBody User obj){
         obj = service.insert(obj);
-        return ResponseEntity.ok().body(obj);
+
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequestUri()
+                .path("/{id}")
+                .buildAndExpand(obj.getId())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(obj);
     }
 }
