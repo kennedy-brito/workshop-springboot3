f762153a74e5732adc03e19538cff84ed6a159fb
kennedy_brito <willian.kennedy.brito@gmail.com>
2024-12-03 15:01:50 -0300

    feat: add User find by id exception handling

--- src/main/java/com/kennedy/course/servicies/UserService.java
+++ src/main/java/com/kennedy/course/servicies/UserService.java
@@ -1,6 +1,7 @@
 package com.kennedy.course.servicies;
 
 import com.kennedy.course.entities.User;
+import com.kennedy.course.servicies.exceptions.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 import com.kennedy.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,7 +22,7 @@ public class UserService {
     public User findById(Long id){
         Optional<User> obj = repository.findById(id);
 
-        return obj.get();
+        return obj.orElseThrow( ()-> new ResourceNotFoundException(id));
     }
 
     public User insert(User user){
