008fa8d08ead9455211a152ac03502c6bcc3fd11
kennedy_brito <willian.kennedy.brito@gmail.com>
2024-12-03 13:58:54 -0300

    feat: add User insert method

--- src/main/java/com/kennedy/course/resources/UserResource.java
+++ src/main/java/com/kennedy/course/resources/UserResource.java
@@ -5,10 +5,7 @@ import com.kennedy.course.servicies.UserService;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,5 +29,11 @@ public class UserResource {
         User user = service.findById(id);
 
         return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj){
+        obj = service.insert(obj);
+        return ResponseEntity.ok().body(obj);
     }
 }
